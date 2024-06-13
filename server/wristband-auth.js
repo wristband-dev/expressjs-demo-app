2078acaae2801ae66111e3f100b893f646c7e0fe
Jim Verducci <jim@wristband.dev>
2024-06-13 15:18:52 -0700

    Updating the Wristband SDK version to 1.1.0

--- server/wristband-auth.js
+++ server/wristband-auth.js
@@ -10,6 +10,8 @@ const tenantDomain = process.env.DOMAIN_FORMAT === 'VANITY_DOMAIN' ? '{tenant_do
 const wristbandAuth = createWristbandAuth({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
+  // NOTE: If deploying your own app to production, do not disable secure cookies.
+  dangerouslyDisableSecureCookies: true,
   loginStateSecret: '7ffdbecc-ab7d-4134-9307-2dfcc52f7475',
   loginUrl: `http://${tenantDomain}${INVOTASTIC_HOST}/api/auth/login`,
   redirectUri: `http://${tenantDomain}${INVOTASTIC_HOST}/api/auth/callback`,
