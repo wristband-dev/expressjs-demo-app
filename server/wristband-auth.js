1af62c741787e1c322c8e88ed450a1a3c672fe9b
Jim Verducci <jim@wristband.dev>
2025-05-08 12:40:50 -0700

    Streamline demo app

--- server/wristband-auth.js
+++ server/wristband-auth.js
@@ -2,23 +2,19 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { createWristbandAuth } = require('@wristband/express-auth');
-
-const { INVOTASTIC_HOST } = require('./utils/constants');
-
-const tenantDomain = process.env.DOMAIN_FORMAT === 'VANITY_DOMAIN' ? '{tenant_domain}.' : '';
+const { INVOTASTIC_HOST, LOGIN_STATE_COOKIE_SECRET } = require('./utils/constants');
 
 const wristbandAuth = createWristbandAuth({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   // NOTE: If deploying your own app to production, do not disable secure cookies.
   dangerouslyDisableSecureCookies: true,
-  loginStateSecret: '7ffdbecc-ab7d-4134-9307-2dfcc52f7475',
-  loginUrl: `http://${tenantDomain}${INVOTASTIC_HOST}/api/auth/login`,
-  redirectUri: `http://${tenantDomain}${INVOTASTIC_HOST}/api/auth/callback`,
-  rootDomain: INVOTASTIC_HOST,
+  loginStateSecret: LOGIN_STATE_COOKIE_SECRET,
+  loginUrl: `http://${INVOTASTIC_HOST}/api/auth/login`,
+  redirectUri: `http://${INVOTASTIC_HOST}/api/auth/callback`,
   useCustomDomains: false,
-  useTenantSubdomains: process.env.DOMAIN_FORMAT === 'VANITY_DOMAIN',
-  wristbandApplicationVanityDomain: process.env.APPLICATION_DOMAIN,
+  useTenantSubdomains: false,
+  wristbandApplicationVanityDomain: process.env.APPLICATION_VANITY_DOMAIN,
 });
 
 module.exports = wristbandAuth;
